727869399d17d6242a3c569f60510025e75e9e02
Rey <reycrittenden@yahoo.com>
2020-02-24 10:35:28 -0600

    Initial Commit

---
+++ MarioAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioAnimations
+//
+//  Created by Crittenden, Rey  on 2/24/20.
+//  Copyright © 2020 Crittenden, Rey . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

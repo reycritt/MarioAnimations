aa717f5b9cfeb2e09e3eae5c1cc290b0b92c1c61
Rey <reycrittenden@yahoo.com>
2020-02-24 23:22:56 -0600

    Drag and Pinch gestures

--- MarioAnimations/ViewController.swift
+++ MarioAnimations/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+//import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Printed")
     }
     
-    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {//On action of the gesture, rather than the image, will do something (in this instance, will drag around the screen); since all 3 images/gestures will move the same, they will all be connected to the same function
-        let location = sender.location(in: view)
+    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {//Upon using a gesture on an image, do an action
+        let location = sender.location(in: view)//"sender" is the gesture, "location" is where the gesture location, "view" is where the gestures are located
         print("Location x: \(location.x), y: \(location.y)")
+        let kartView = sender.view!//Gets the view (ie the kart/image) that a gesture is applied to
+        kartView.center = location//Sets the view's center to location, aka the location of the gesture (where a finger is placed on the screen)
     }
     
-
+    @IBAction func didPinchKart(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale//Scales based on pinch
+        print("Scale: \(scale)")
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)//Changes the scale of the view/image based on sender.scale
+    }
+    
 }
 
+/*
+ Panning - moving anywhere on the screen
+ To allow images to be tappable, make sure they are eneabled in the storyboard
+ Hold option on simulator to show 2 fingers. Hold shift while the fingers are together to move them. Let go of shift and drag away to mimic a pinch
+ */
